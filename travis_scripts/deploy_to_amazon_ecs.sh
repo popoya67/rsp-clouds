7cd55e81a4441e56a4a2083765ad66b626ce430d
popoya67 <popoya67@naver.com>
2019-10-30 15:16:22 +0900

    accesskey, secretkey 재할당

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,6 +1,6 @@
 echo "Launching IN AMAZON ECS"
 # ecs-cli configure --region us-west-1 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster spmia-tmx-dev
-ecs-cli configure profile --profile-name "default" --access-key AKIAJ7347PDJA65UD6YQ --secret-key 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
+ecs-cli configure profile --profile-name "default" --access-key AKIAJMYXHTQCO7ZMKWSA --secret-key 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
 ecs-cli configure --cluster rsp --default-launch-type EC2 --region ap-northeast-2 --config-name default
 ecs-cli compose --file docker/aws-dev/docker-compose.yml up
 rm -rf ~/.ecs
